0e009e3fb422082e81973d9a6dc838f8b1488cea
Nathan Schreiber <nathanschreiber76@gmail.com>
2022-08-18 17:59:55 -0400

    Create outline of UI
    Created the basic outline of the UI without changing JS much at all.
    This commit was made to ensure safety of the games functionality before
    adding changing border color for round choices.

--- js/script.js
+++ js/script.js
@@ -18,12 +18,17 @@ const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 const reset = document.querySelector('#reset');
+const roundChoices = document.querySelector('.roundChoices');
 
 // Gets divs and sets them to variables
 const roundResults = document.querySelector('.roundResults');
 const finalResults = document.querySelector('.finalResults');
 const choiceButtons = document.querySelector('.choiceButtons');
 const currentScore = document.querySelector(".currentScore");
+
+// Makes borders not appear before choices are made
+roundResults.style.display = "none";
+finalResults.style.display = "none"
 
 // Plays round with playerSelection as button clicked,
 // then uses checkWinner to declare winner
@@ -57,34 +62,40 @@ let computerScore = 0;
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "Rock" && computerSelection === "Scissors") {
         roundResults.textContent = "You win, Rock beats Scissors!";
+        roundResults.style.display = "unset";
         win = true;
         tie = null;
         playerScore++;
         // return roundResult;
     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
         roundResults.textContent = "You win, Scissors beats Paper!";
+        roundResults.style.display = "unset";
         win = true;
         tie = null;
         playerScore++;
         // return roundResult;
     } else if (playerSelection === "Paper" && computerSelection === "Rock") {
         roundResults.textContent =  "You win, Paper beats Rock!";
+        roundResults.style.display = "unset";
         win = true;
         tie = null;
         playerScore++;
         // return roundResult;
     } else if (playerSelection === computerSelection) {
         roundResults.textContent =  "You tied, try again!";
+        roundResults.style.display = "unset";
         tie = true;
         win = null;
         // return roundResult;
     } else {
         roundResults.textContent =  `You lose, ${computerSelection} beats ${playerSelection}.`;
+        roundResults.style.display = "unset";
         win = false;
         tie = null;
         computerScore++;
     }
 
+    roundChoices.textContent = `You chose ${playerSelection} and the computer chose ${computerSelection}`;
     currentScore.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
 };
 
@@ -92,8 +103,9 @@ function playRound(playerSelection, computerSelection) {
 function scoreReset () {
     playerScore = 0;
     computerScore = 0;
-    roundResults.textContent = "";
-    finalResults.textContent = "";
+    roundChoices.textContent = "";
+    roundResults.style.display = "none";
+    finalResults.style.display = "none";
     currentScore.textContent = "Player: 0 Computer: 0";
     choiceButtons.style.pointerEvents = "auto";
 };
@@ -105,9 +117,10 @@ function checkWinner(playerScore, computerScore) {
     if (playerScore === 5) {
         finalResults.textContent = `You won ${playerScore} to ${computerScore}, good job!`;
         choiceButtons.style.pointerEvents = "none";
-
+        finalResults.style.display = "unset";
     } else if (computerScore === 5) {
         finalResults.textContent = `You lost ${playerScore} to ${computerScore}, try again!`;
         choiceButtons.style.pointerEvents = "none";
+        finalResults.style.display = "unset";
     }
 };
